6e05140bb5455e8fd683e6497e12aedfad1945d3
juaky <juaky73@gmail.com>
2017-03-09 11:47:53 +0100

    Modificada la llamada a las imágenes para poder exportarlo a .jar

--- src/Principal.java
+++ src/Principal.java
@@ -1,26 +1,43 @@
 import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 
-public class Principal extends GraphicsProgram {
+public class Principal extends GraphicsProgram   {
     public void run () {
         Bola bola;
         ArrayList<Bola> misbolas = new ArrayList<Bola>();  //Conjunto de emojis normales
         ArrayList<Bola> bolasasesinas = new ArrayList<Bola>(); //Conjunto de emojis zoombies
         this.setSize(500, 500);
         Random random= new Random();
+        try {
+            BufferedImage img = ImageIO.read(getClass().getResourceAsStream("images/emojiasesino.png"));
 
-        //Añadir un emoji asesino
-        bolasasesinas.add(new Bola(random.nextInt(getWidth()-101),random.nextInt(getHeight()-101),new GImage("../images/emojiasesino.png")));
-        add(bolasasesinas.get(0).getImagen(),bolasasesinas.get(0).getPosx(),bolasasesinas.get(0).getPoxy());
-
+            //Añadir un emoji asesino
+            bolasasesinas.add(new Bola(random.nextInt(getWidth() - 101), random.nextInt(getHeight() - 101), new GImage(img)));
+            //bolasasesinas.add(new Bola(random.nextInt(getWidth()-101),random.nextInt(getHeight()-101),new GImage("./images/emojiasesino.png")));
+            add(bolasasesinas.get(0).getImagen(), bolasasesinas.get(0).getPosx(), bolasasesinas.get(0).getPoxy());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         //Añadir emojis normales
-        for (int i = 0; i <20 ; i++) {
-            int numimagen= random.nextInt(9)+1;
-            misbolas.add(new Bola(random.nextInt(getWidth()-101),random.nextInt(getHeight()-101),new GImage("../images/emoji" + numimagen +".png")));
+        try {
+
+
+            for (int i = 0; i < 20; i++) {
+                int numimagen = random.nextInt(9) + 1;
+                BufferedImage img = ImageIO.read(getClass().getResourceAsStream("images/emoji" + numimagen + ".png"));
+                //misbolas.add(new Bola(random.nextInt(getWidth()-101),random.nextInt(getHeight()-101),new GImage("./images/emoji" + numimagen +".png")));
+                misbolas.add(new Bola(random.nextInt(getWidth()-101),random.nextInt(getHeight()-101),new GImage(img)));
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
         }
         for (Bola mibola:misbolas) {
             add(mibola.getImagen(),mibola.getPosx(),mibola.getPoxy());
@@ -81,7 +98,15 @@ public class Principal extends GraphicsProgram {
             misbolas.remove(tocada);
             GImage pic = tocada.getImagen();
             pic.getParent().remove(pic);
-            tocada.setImagen(new GImage("../images/emojiasesino.png"));
+            try {
+                BufferedImage img = ImageIO.read(getClass().getResourceAsStream("images/emojiasesino.png"));
+                tocada.setImagen(new GImage(img));
+                //tocada.setImagen(new GImage("./images/emojiasesino.png"));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+
             add(tocada.getImagen(),tocada.getPosx(),tocada.getPoxy());
             bolasasesinas.add(tocada);
         }
@@ -95,12 +120,19 @@ public class Principal extends GraphicsProgram {
      */
     public void partidaFinalizada(){
         pause(400);
-        GImage zoombiefinal = new GImage("../images/zoombie.png");
+        try {
+            BufferedImage img = ImageIO.read(getClass().getResourceAsStream("images/zoombie.png"));
+            GImage zoombiefinal = new GImage(img);
+
+        //GImage zoombiefinal = new GImage("./images/zoombie.png");
         add(zoombiefinal,(getWidth()-zoombiefinal.getWidth())/2,(getHeight()-zoombiefinal.getHeight())/2);
         zoombiefinal.setBounds(0,0,50,50);
         while (zoombiefinal.getWidth()<getWidth()){
             zoombiefinal.setBounds(0,0,zoombiefinal.getWidth()+50,zoombiefinal.getHeight()+50);
             pause(30);
+        }
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
